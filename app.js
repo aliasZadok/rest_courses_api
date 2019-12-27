0414dceba8e1770f1f7bdb3fc691abd722687802
aliasZadok <lfstone3@gmail.com>
2019-12-27 22:07:04 +0530

    update app.js

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET');
     return res.status(200).end();
   }
+  next();
 });
 app.use('/api', routes);
 
